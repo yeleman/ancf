f5bc061bc38c8a4bd8ba6658a6fedf5f91d65e63
Alou Dolo <aloud7@gmail.com>
2013-02-18 11:32:06 +0000

    correction de settings

--- anm_site/settings.py
+++ anm_site/settings.py
@@ -42,7 +42,7 @@ MEDIA_ROOT = os.path.join(ROOT_DIR, 'media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -54,10 +54,6 @@ STATIC_ROOT = ROOT_DIR + '/static/'
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
